e198a5119196848179d5927a95788ef5e7bab1dd
Ray's Git <lancelot53.ra@gmail.com>
2022-01-15 21:56:23 -0500

    Completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end   # code here
+
+  def add_student(name, grade)
+    if roster.key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+#   def sort
+#     roster.sort_by{|grade, name|  name}.to_h.sort.to_h#map #{ |grade, name| "#{grade} name.sort"}
+#     #roster.sort|grade,name| name.length <=> b.length 
+#   end
+def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
